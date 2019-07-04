4b64b13b3ff63a781c9c66189014ab9387541e7a
Edi Modrić <edi.modric@gmail.com>
2019-07-04 15:36:28 +0200

    Install Netgen Layouts

---
+++ src/Controller/HomepageController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomepageController extends AbstractController
+{
+    /**
+     * @Route("/", name="homepage")
+     */
+    public function homepage()
+    {
+        return $this->render('homepage/homepage.html.twig');
+    }
+}
