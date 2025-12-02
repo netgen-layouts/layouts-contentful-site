5bd668456545e982edc6ab1867e9bfbe862fa6da
Edi Modrić <edi.modric@gmail.com>
2025-12-02 19:29:09 +0100

    Upgrade project to Symfony 7.4 and Netgen Layouts 2

--- src/Controller/Homepage.php
+++ src/Controller/Homepage.php
@@ -3,14 +3,13 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
 
 final class Homepage extends AbstractController
 {
-    /**
-     * @Route("/", name="homepage")
-     */
-    public function __invoke()
+    #[Route('/', name: 'homepage')]
+    public function __invoke(): Response
     {
         return $this->render('homepage/homepage.html.twig');
     }
